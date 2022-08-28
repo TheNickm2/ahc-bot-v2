840d8ac3a5f5683812f60049729602754171e1ec
Nick Mykkanen <nmykkanen@vividfront.com>
2022-08-27 21:48:51 -0500

    add initial command UI

--- src/commands/auction.ts
+++ src/commands/auction.ts
@@ -3,6 +3,7 @@ import {
   CommandInteraction,
   MessageActionRow,
   MessageButton,
+  MessageEmbed,
   Modal,
   TextInputComponent,
 } from 'discord.js';
@@ -14,14 +15,41 @@ import { saveAuctionLot } from '@/database';
 
 const DEFAULT_PERMISSIONS_INTEGER = 1099511627782;
 
+const POST_LOTS_BUTTON_ID = 'post-lots';
+const UPDATE_LOTS_BUTTON_ID = 'update-lots';
+const START_AUCTION_BUTTON_ID = 'start-auction';
+
 export const auctionCommand = {
   createCommand: () => {
     return new SlashCommandBuilder()
       .setName('auction')
-      .setDescription('Auction Management')
+      .setDescription('Auction Management | Officers Only')
       .setDefaultMemberPermissions(DEFAULT_PERMISSIONS_INTEGER);
   },
   executeCommand: async (interaction: CommandInteraction) => {
+    const postLotsButton = new MessageButton()
+      .setCustomId(POST_LOTS_BUTTON_ID)
+      .setLabel('Post Lots')
+      .setStyle('DANGER');
+
+    const updateLotsButton = new MessageButton()
+      .setCustomId(UPDATE_LOTS_BUTTON_ID)
+      .setLabel('Update Lots')
+      .setStyle('SECONDARY');
+    const startAuctionButton = new MessageButton()
+      .setCustomId(START_AUCTION_BUTTON_ID)
+      .setLabel('Start Auction')
+      .setStyle('SUCCESS');
+    const actionRow = new MessageActionRow().addComponents([
+      postLotsButton,
+      updateLotsButton,
+      startAuctionButton,
+    ]);
+    await interaction.reply({
+      components: [actionRow],
+      content: '**Auction Management**',
+      ephemeral: true,
+    });
   },
   registerEvents: (emitter: EventEmitter) => {},
 };
