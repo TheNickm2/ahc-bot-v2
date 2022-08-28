9c01298e89977dba26c35b8490a2207a9c7a8e39
Nick Mykkanen <nmykkanen@vividfront.com>
2022-08-27 19:22:40 -0500

    add separate historical lot db collection

--- src/commands/auction.ts
+++ src/commands/auction.ts
@@ -10,6 +10,7 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import type { EventEmitter } from 'events';
 import { isStringUrl, Logger } from '@/utils';
 import { embedAuctionLot } from '@/embeds';
+import { saveAuctionLot } from '@/database';
 
 const DEFAULT_PERMISSIONS_INTEGER = 1099511627782;
 
@@ -21,6 +22,15 @@ export const auctionCommand = {
       .setDefaultMemberPermissions(DEFAULT_PERMISSIONS_INTEGER);
   },
   executeCommand: async (interaction: CommandInteraction) => {
+    const result = await saveAuctionLot({
+      id: interaction.guild?.id ?? '01',
+      title: interaction.user.username,
+      description: interaction.user.toString(),
+      startingBid: 696969,
+      currentLeader: interaction.user.id,
+      currentBid: 5256000
+    });
+    console.log(result);
     await interaction.reply({
       ephemeral: true,
       content: 'response',
---
+++ src/database/historicalLot.ts
@@ -0,0 +1,51 @@
+import { Schema, model, connection } from 'mongoose';
+
+export interface IHistoricalLot {
+  title: string;
+  description: string;
+  startingBid: number;
+  winningBid: number;
+  winnerId: string;
+  id: string;
+}
+
+const historicalLotSchema = new Schema<IHistoricalLot>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  startingBid: { type: Number, required: true },
+  winningBid: { type: Number, required: true },
+  winnerId: { type: String, required: true },
+  id: { type: String, required: true, unique: true },
+});
+
+const HistoricalLot = model<IHistoricalLot>(
+  'HistoricalLot',
+  historicalLotSchema,
+);
+
+export async function getHistoricalLot(lotId: string) {
+  try {
+    const result = await HistoricalLot.findOne({ id: lotId });
+    if (result) {
+      return result;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function saveHistoricalLot(lot: IHistoricalLot) {
+  try {
+    const existingLot = await getHistoricalLot(lot.id);
+    if (existingLot) {
+      existingLot.overwrite(lot);
+      await existingLot.save();
+      return existingLot;
+    } else {
+      const result = await HistoricalLot.create(lot);
+      return result;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
