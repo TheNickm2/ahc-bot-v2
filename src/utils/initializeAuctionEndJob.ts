cca24c178c415f9ae4242fc9e9e98aa12e60ff8b
Nick Mykkanen <th3nickm2@gmail.com>
2022-08-28 23:10:44 -0500

    store auction channel ID

--- src/utils/initializeAuctionEndJob.ts
+++ src/utils/initializeAuctionEndJob.ts
@@ -1,41 +1,54 @@
 import { getRedisKeyValue, setRedisKeyValue } from '@/utils';
 import { endAuction } from '@/utils/endAuction';
 import { Logger } from '@/utils/logger';
+import { Client, TextChannel } from 'discord.js';
 import schedule from 'node-schedule';
 import Sugar from 'sugar';
 
 export let isAuctionActive = false;
 
-export async function activateAuction(endDate: Date) {
+export async function activateAuction(
+  endDate: Date,
+  client: Client,
+  channel: TextChannel,
+) {
   try {
     const redisDatePromise = setRedisKeyValue(
       'auctionEndDate',
       endDate.toUTCString(),
     );
     const redisActivePromise = setRedisKeyValue('auctionActive', 'true');
-    const initJobPromise = initializeAuctionEndJob(endDate);
-    const [dateResult, activateResult, initJobResult] = await Promise.all([
-      redisDatePromise,
-      redisActivePromise,
-      initJobPromise,
-    ]);
-    if (!dateResult || !activateResult || !initJobPromise) {
+    const redisChannelIdPromise = setRedisKeyValue(
+      'auctionChannelId',
+      channel.id,
+    );
+    const initJobPromise = initializeAuctionEndJob(endDate, client);
+    const [dateResult, activateResult, initJobResult, channelIdResult] =
+      await Promise.all([
+        redisDatePromise,
+        redisActivePromise,
+        initJobPromise,
+        redisChannelIdPromise,
+      ]);
+    if (!dateResult || !activateResult || !channelIdResult || !initJobPromise) {
       Logger.error(
         `Failed to activate auction. Date: ${dateResult}, Activate: ${activateResult}, Create Job Result: ${initJobResult}`,
       );
     }
-    return Boolean(dateResult && activateResult && initJobPromise);
+    return Boolean(
+      dateResult && activateResult && channelIdResult && initJobPromise,
+    );
   } catch (err) {
     Logger.error(err);
   }
 }
 
-export async function initializeAuctionEndJob(endDate: Date) {
+export async function initializeAuctionEndJob(endDate: Date, client: Client) {
   try {
     Logger.info('Initializing auction end job');
     const job = schedule
       .scheduleJob(endDate, async () => {
-        await endAuction();
+        await endAuction(client);
       })
       .addListener('success', () => Logger.info('Auction ended successfully'))
       .addListener('error', (err) =>
